91537e80fa5c5632a44acdb7719c0a7738ebd937
graduatemcoaug-arch <graduate.mcoa.ug@gmail.com>
2026-09-05 10:19:14 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -131,29 +131,6 @@ async function approveApp(id) {
   loadApplications();
 }
 
-async function sendEmail(to, subject, html) {
-  try {
-    const res = await fetch("https://oxcefktkqqjxmekuyvwd.supabase.co/functions/v1/send-email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${SUPABASE_ANON_KEY}`,
-        "apikey": SUPABASE_ANON_KEY,
-      },
-      body: JSON.stringify({ to, subject, html }),
-    });
-    const result = await res.json();
-    if (!res.ok) {
-      console.error("Email send failed:", result);
-      return false;
-    }
-    return true;
-  } catch (err) {
-    console.error("Email send failed:", err);
-    return false;
-  }
-}
-
 function escapeHtmlAd(str) {
   const div = document.createElement("div");
   div.textContent = str ?? "";
