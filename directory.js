09988f4e59b281f31fc651cad6d3c673a87d722c
graduatemcoaug-arch <graduate.mcoa.ug@gmail.com>
2026-09-08 14:42:15 +0300

    Update directory.js

--- directory.js
+++ directory.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function checkSession() {
   const { data: { session } } = await supabaseClient.auth.getSession();
-  if (session) showDashboard();
+  if (session) enforceRole(["Secretariat"], showDashboard);
 }
 
 async function login(e) {
@@ -25,7 +25,7 @@ async function login(e) {
     note.style.color = "#B3261E";
     return;
   }
-  showDashboard();
+  enforceRole(["Secretariat"], showDashboard);
 }
 
 async function logout() {
